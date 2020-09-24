2b141f4a32a8679cdabcaf8194ca669ee1891150
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2020-09-24 17:52:39 +0100

    update

--- runme_eid.sh
+++ runme_eid.sh
@@ -7,7 +7,7 @@
 CONFIG="tune0"
 #DATAPATH="/home/user/imperial_trees"
 #DATAPATH="/home/user/imperial_new_trees"
-DATAPATH="/vols/cms/icenet/data
+DATAPATH="/vols/cms/icenet/data"
 
 python eid_train.py --config $CONFIG --datapath $DATAPATH --datasets 0 #,1 #,2,3,4,5,6
 python eid_eval.py  --config $CONFIG --datapath $DATAPATH --datasets 0 #,1 #,2,3,4,5,6
