f0491d20efbd96446dec0900f8a7c72426845cce
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2020-10-18 02:27:49 +0100

    update

--- runme_deep_eid.sh
+++ runme_deep_eid.sh
@@ -6,15 +6,14 @@
 
 CONFIG="tune0_deep_dev"
 
-DATAPATH="/home/user/imperial_trees/2019sep13"
+#DATAPATH="/home/user/imperial_trees/2019sep13"
 #DATAPATH="/home/user/imperial_new_trees"
 #DATAPATH="/home/user/imperial_old_trees"
-#DATAPATH="/vols/cms/icenet/data/2020Aug21"
-
+DATAPATH="/vols/cms/icenet/data/2020Oct16/"
 
 # Use * or other glob wildcards for filenames
 
-python ./analysis/eid_deep_train.py --config $CONFIG --datapath $DATAPATH --datasets "output_{0,1}"
+python ./analysis/eid_deep_train.py --config $CONFIG --datapath $DATAPATH --datasets "output_*" # output_{0,1}
 python ./analysis/eid_eval.py --config $CONFIG --datapath $DATAPATH --datasets "output_0"
 #python ./analysis/eid_visual_tensors.py --config $CONFIG --datapath $DATAPATH --datasets "output_0"
 
