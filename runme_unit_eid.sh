8fb54a53b3d6346bf3a7e7cdbc98fb30f3e8473b
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2020-10-21 02:35:40 +0100

    update

--- runme_unit_eid.sh
+++ runme_unit_eid.sh
@@ -4,24 +4,23 @@
 #
 # Run with: source runme.sh
 
-#DATAPATH="/home/user/imperial_trees/2019sep13"
+DATAPATH="/home/user/imperial_trees/2019sep13"
 #DATAPATH="/home/user/imperial_new_trees"
 #DATAPATH="/home/user/imperial_old_trees"
-DATAPATH="/vols/cms/icenet/data/2020Oct16/"
+#DATAPATH="/vols/cms/icenet/data/2020Oct16/"
 
 
 # Use * or other glob wildcards for filenames
-CONFIG="unit_rw_none"
-python ./analysis/eid_train.py --config $CONFIG --datapath $DATAPATH --datasets "output_{0,1,2,3,4}" #,1 #,2,3,4,5,6
-python ./analysis/eid_eval.py  --config $CONFIG --datapath $DATAPATH --datasets "output_5" #,1 #,2,3,4,5,6
+#CONFIG="unit_rw_none"
+#python ./analysis/eid_train.py --config $CONFIG --datapath $DATAPATH --datasets "output_{0,1,2,3,4}" #,1 #,2,3,4,5,6
+#python ./analysis/eid_eval.py  --config $CONFIG --datapath $DATAPATH --datasets "output_0" #,1 #,2,3,4,5,6
 
 CONFIG="unit_rw_background"
 python ./analysis/eid_train.py --config $CONFIG --datapath $DATAPATH --datasets "output_{0,1,2,3,4}" #,1 #,2,3,4,5,6
-python ./analysis/eid_eval.py  --config $CONFIG --datapath $DATAPATH --datasets "output_5" #,1 #,2,3,4,5,6
+python ./analysis/eid_eval.py  --config $CONFIG --datapath $DATAPATH --datasets "output_0" #,1 #,2,3,4,5,6
 
 CONFIG="unit_rw_signal"
 python ./analysis/eid_train.py --config $CONFIG --datapath $DATAPATH --datasets "output_{0,1,2,3,4}" #,1 #,2,3,4,5,6
-python ./analysis/eid_eval.py  --config $CONFIG --datapath $DATAPATH --datasets "output_5" #,1 #,2,3,4,5,6
+python ./analysis/eid_eval.py  --config $CONFIG --datapath $DATAPATH --datasets "output_0" #,1 #,2,3,4,5,6
 
 #python ./analysis/eid_visual_tensors.py --config $CONFIG --datapath $DATAPATH --datasets "output_0" #,1 #,2,3,4,5,6
-
