357995bd35b54a65a1017b1deac2e5af8c548f53
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2021-01-07 12:05:23 +0000

    update

---
+++ tests/gpu-check.py
@@ -0,0 +1,9 @@
+# Test GPU support
+
+import tensorflow
+import torch
+
+from tensorflow.python.client import device_lib
+
+print(f'   >>> tensorflow: {tensorflow.test.is_gpu_available()} {device_lib.list_local_devices()}')
+print(f'   >>> torch:      {torch.cuda.get_device_name(0)} {torch.cuda.is_available()}')
