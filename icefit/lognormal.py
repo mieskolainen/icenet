3ca30ea023438f2e4fd361135be597eb96252f89
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2021-04-08 00:25:05 +0300

    clean up

--- icefit/lognormal.py
+++ icefit/lognormal.py
@@ -13,10 +13,10 @@ def lognormal_param(m, v, inmode='mean'):
     """
     Compute log-normal distribution \mu and \sigma parameters from
     the target mean (or median) m and variance v
-    
+
     \mu and \sigma follow the Wikipedia convention:
     https://en.wikipedia.org/wiki/Log-normal_distribution
-    
+
     Args:
         m      : mean (or median or mode) value of the target log-normal pdf
         v      : variance value of the target log-normal pdf
@@ -26,7 +26,7 @@ def lognormal_param(m, v, inmode='mean'):
         """ CLOSED FORM SOLUTION """
         mu    = np.log(m**2/np.sqrt(v + m**2))
         sigma = np.sqrt(np.log(v/m**2 + 1))
-    
+
     elif inmode == 'median':
         """ CLOSED FORM SOLUTION """
         mu    = np.log(m)
@@ -44,11 +44,11 @@ def lognormal_param(m, v, inmode='mean'):
 
     else:
         raise Except('lognormal_param: Unknown inmode')
-    
-    print(f'lognormal_param:') 
+
+    print(f'lognormal_param:')
     print(f'  - Input:     m = {m:0.5f}, v = {v:0.5f}, target = {inmode}')
     print(f'  - Obtained parameters: mu = {mu:0.5f}, sigma = {sigma:0.5f}')
-    
+
     return mu, sigma
 
 
@@ -56,7 +56,7 @@ def rand_lognormal(m, v, N, inmode='mean'):
     """
     Generate random numbers from log-normal density with
     the distribution target mean (or median) m and variance v
-    
+
     Args:
         m      : mean (or median or mode) value of the target log-normal pdf
         v      : variance value of the target log-normal pdf
@@ -67,12 +67,12 @@ def rand_lognormal(m, v, N, inmode='mean'):
 
     # Standard transform
     y = np.exp(mu + sigma * np.random.randn(N))
-    
+
     return y
 
 
 def test_lognormal():
-    
+
     import pytest
 
     N  = int(1e7)
@@ -89,25 +89,24 @@ def test_lognormal():
     fig, ax = plt.subplots(nrows=len(delta_val), ncols=len(modes), figsize=(12,6))
     for i in range(len(delta_val)):
         delta = delta_val[i]
-            
         for j in range(len(modes)):
-               
+
             # Generate log-normally distributed variables
             b   = rand_lognormal(m=b0, v=(delta*b0)**2, N=N, inmode=modes[j])
-            
+
             # Computete statistics
-            mu  = np.mean(b)
-            med = np.median(b)
-            std = np.std(b)
-            
+            mean = np.mean(b)
+            med  = np.median(b)
+            std  = np.std(b)
+
             # Find mode
             bins = np.linspace(0, 5*b0, 300)
             counts, xval = np.histogram(b, bins)
             mode  = xval[np.argmax(counts)]
 
-            label = f'b_0 = {b0:0.1f}, \delta = {delta:0.2f} :: (mean,med,mode) = ({mu:0.2f}, {med:0.2f}, {mode:0.2f}), std = {std:0.2f}, std/mean = {std/mu:0.2f}, std/med = {std/med:0.2f}, std/mode = {std/mode:0.2f}'
+            label = f'b_0 = {b0:0.1f}, \delta = {delta:0.2f} :: (mean,med,mode) = ({mean:0.2f}, {med:0.2f}, {mode:0.2f}), std = {std:0.2f}, std/mean = {std/mean:0.2f}, std/med = {std/med:0.2f}, std/mode = {std/mode:0.2f}'
             print(f'{label} \n')
-            
+
             if (i == 0):
                 ax[i,j].set_title(f'<{modes[j]}> as the target')
 
@@ -117,6 +116,5 @@ def test_lognormal():
             ax[i,j].set_xlabel('$b$')
 
         print('----------------------------------------------------------')
-        
     plt.show()
 
