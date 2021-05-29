7ad5e7e9a7459e7964b39ff7b98080fee87eb7a0
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2021-05-29 12:46:09 +0300

    Hessian code

--- icefit/jacobian.py
+++ icefit/jacobian.py
@@ -1,4 +1,4 @@
-# Autograd Jacobians and related
+# Torch autograd Jacobians, Hessians and related
 # 
 # m.mieskolainen@imperial.ac.uk, 2021
 
@@ -64,6 +64,122 @@ def get_jacobian(inputs, func, **kwargs):
         J[i,:,:] = x.grad.data
 
     return J
+
+
+def get_full_hessian(loss, param):
+    """
+    Compute the full Hessian matrix of a neural net (can be very slow)
+    
+    https://www.cs.toronto.edu/~rgrosse/courses/csc2541_2021/readings/L02_Taylor_approximations.pdf
+    
+    Args:
+        loss: neural net loss function
+        param: neural net parameters
+    """
+
+    N = param.numel()
+    H = torch.zeros(N,N)
+    
+    loss_grad = torch.autograd.grad(loss, param, create_graph=True, retain_graph=True, only_inputs=True)[0].view(-1)
+    for i in tqmd(range(N)):
+        grad2 = torch.autograd.grad(loss_grad[i], param, create_graph=False, retain_graph=True, only_inputs=True)
+        H[i] = grad2[0].view(-1)
+    return H
+
+
+"""
+def get_hessian(inputs, func, **kwargs):
+    
+    The Hessian matrix of a function of is the Jacobian matrix of the gradient of the function f.
+    
+    H(f(x)) = J(grad(f(x))
+    
+    Note that sometimes Hessian is approximated using a product of Jacobians as JJ^T
+
+    TBD: Do we assume derivatives commute here (c.f. Grasmann variables) ?
+""" 
+
+
+def observed_fisher_info(logL, inputs, **kwargs):
+    """
+    Observed Fisher information -- the negative of the second derivative
+    (Hessian matrix) of the log-likelihood function.
+
+    The observed information is typically evaluated at the maximum-likelihood estimate of theta.
+    
+    "Fisher information represents the curvature of the relative entropy"
+    
+    Args:
+        logL:   The log-likelihood function l(theta|X1,...X_n) = sum_{i=1}^n log f(X_i|theta),
+                with X_i being the independent random observations
+        inputs: log-likelihood function input
+    
+    References:
+        https://en.wikipedia.org/wiki/Observed_information
+        https://en.wikipedia.org/wiki/Optimal_design
+
+        Efron, B.; Hinkley, D.V. (1978).
+        "Assessing the accuracy of the maximum likelihood estimator:
+         Observed versus expected Fisher Information". Biometrika. 65 (3): 457–487.
+    """
+    I = - torch.autograd.functional.hessian(func=logL, inputs=inputs).squeeze()
+
+    return I
+
+
+def test_hessians(EPS=1e-4):
+    """
+    Test Hessian matrix computations
+    """
+
+    def func_f1(x):
+        # R^3 -> R
+        #
+        # Input dim: n_batches x 3
+        #
+        return x[:,0]**2 + x[:,1]**2 + x[:,2]**2
+
+    def H_1(x):
+        # Hessian for func_f1
+        return torch.tensor([[2,  0,  0],
+                             [0,  2,  0],
+                             [0,  0,  2]])
+
+    def func_f2(x):
+        # R^3 -> R
+        #
+        # Input dim: n_batches x 3
+        return torch.sin(x[:,0] * x[:,1] * x[:,2])
+
+    def H_2(X):
+        # Hessian for func_f2
+        x = X[:,0]
+        y = X[:,1]
+        z = X[:,2]
+
+        sin = torch.sin
+        cos = torch.cos
+
+        return torch.tensor([[-y**2*z**2*sin(x*y*z), -x*y*z**2*sin(x*y*z) + z*cos(x*y*z), -x*y**2*z*sin(x*y*z) + y*cos(x*y*z)],
+                          [-x*y*z**2*sin(x*y*z) + z*cos(x*y*z), -x**2*z**2*sin(x*y*z), -x**2*y*z*sin(x*y*z) + x*cos(x*y*z)],
+                          [-x*y**2*z*sin(x*y*z) + y*cos(x*y*z), -x**2*y*z*sin(x*y*z) + x*cos(x*y*z), -x**2*y**2*sin(x*y*z)]])
+
+    for i in range(1000):
+
+        ### TEST CASE 1
+        x          = Variable(torch.randn(size=(1,3)))
+        H_auto     = torch.autograd.functional.hessian(func=func_f1, inputs=x).squeeze()
+        H_analytic = H_1(x)
+
+        assert torch.norm(H_auto - H_analytic) < EPS
+
+
+        ### TEST CASE 2
+        x          = Variable(torch.randn(size=(1,3)))
+        H_auto     = torch.autograd.functional.hessian(func=func_f2, inputs=x).squeeze()
+        H_analytic = H_2(x)
+
+        assert torch.norm(H_auto - H_analytic) < EPS
 
 
 def test_dimension_interfaces():
@@ -144,11 +260,11 @@ def test_dimension_interfaces():
             print('\n')
 
 
-def test_jacobians(EPS=1e-6):
-    """
-    Test Jacobians from:
-    https://en.wikipedia.org/wiki/Jacobian_matrix_and_determinant
-    """
+def test_jacobians(EPS=1e-4):
+    
+    #Test Jacobians from:
+    #https://en.wikipedia.org/wiki/Jacobian_matrix_and_determinant
+    
     import pytest
 
     def func_f1(x):
@@ -211,7 +327,7 @@ def test_jacobians(EPS=1e-6):
 
     # Test with random input
     for k in range(100):
-
+        
         ### TEST CASE 1
         x = Variable(torch.randn(size=(1,2)))
 
@@ -254,6 +370,4 @@ def test_jacobians(EPS=1e-6):
         print(J_analytic)
 
         assert torch.norm(J_auto - J_analytic) < EPS
-
-
-
+        
