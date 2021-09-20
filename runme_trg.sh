0f580a565d281f4bc8715fe055d398525e2521b6
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2021-09-21 00:23:20 +0100

    tuning

--- runme_trg.sh
+++ runme_trg.sh
@@ -10,6 +10,6 @@ DATAPATH="/home/user/cernbox/HLT_electrons"
 # Use * or other glob wildcards for filenames
 mkdir ./figs/trg/$CONFIG -p # for output ascii dump
 
-# tee -a redirect output to both a file and to screen
-python ./analysis/trg_train.py --config $CONFIG --datapath $DATAPATH --datasets "none" | tee -a "./figs/trg/$CONFIG/train_output.txt"
-python ./analysis/trg_eval.py  --config $CONFIG --datapath $DATAPATH --datasets "none" | tee -a "./figs/trg/$CONFIG/eval_output.txt"
+# tee redirect output to both a file and to screen
+python ./analysis/trg_train.py --config $CONFIG --datapath $DATAPATH --datasets "none" | tee "./figs/trg/$CONFIG/train_output.txt"
+python ./analysis/trg_eval.py  --config $CONFIG --datapath $DATAPATH --datasets "none" | tee "./figs/trg/$CONFIG/eval_output.txt"
