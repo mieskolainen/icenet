a0bf5900e22e9ae4217761573ae45df235a791d2
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2021-09-20 14:57:06 +0100

    tests

--- tests/test_icemap.py
+++ tests/test_icemap.py
@@ -7,16 +7,19 @@ sys.path.append(".")
 from icenet.tools import iceroot
 
 rootfile = '/home/user/cernbox/HLT_electrons/efftest.root'
+cuts     = 'gen_e1_l1_dr < 0.2 AND gen_e2_l1_dr < 0.2 AND e1_l1_pt >=6 AND e2_l1_pt >= 6'
 
-X = iceroot.load_tree(rootfile=rootfile, tree='tree', entry_stop=None)
+for key in ['tree;1', 'tree;2']:
 
-print(X.ids)
+	print(key)
+	X = iceroot.load_tree(rootfile=rootfile, tree=key, entry_stop=None)
+	#print(X.ids)
 
-cuts = 'gen_e1_l1_dr < 0.2 AND gen_e2_l1_dr < 0.2 AND gen_e1_hlt_dr < 0.2 AND e1_l1_pt >=6 AND e2_l1_pt >= 6'
-y = X[cuts]
-print(f'{X.x.shape[0]} {y.shape[0]} {y.shape[0] / X.x.shape[0]:0.3E}')
+	y1 = X[cuts + " AND gen_e1_hlt_dr < 0.2"]
+	y2 = X[cuts + " AND gen_e2_hlt_dr < 0.2"]
+	
+	print(f'{X.x.shape[0]} {y1.shape[0]} {y1.shape[0] / X.x.shape[0]:0.3E}')
+	print(f'{X.x.shape[0]} {y2.shape[0]} {y2.shape[0] / X.x.shape[0]:0.3E}')
 
+	print('')
 
-cuts = 'gen_e1_l1_dr < 0.2 AND gen_e2_l1_dr < 0.2 AND gen_e1_hlt_dr < 0.2 AND e1_l1_pt >=6 AND e2_l1_pt >= 6'
-y = X[cuts]
-print(f'{X.x.shape[0]} {y.shape[0]} {y.shape[0] / X.x.shape[0]:0.3E}')
