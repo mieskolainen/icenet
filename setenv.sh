9eda7b62b7a5af110c29ba6fdc1772d878a24fc8
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2021-11-29 19:55:25 +0000

    update docs

--- setenv.sh
+++ setenv.sh
@@ -1,3 +1,3 @@
 # Set library paths
-
-export LD_LIBRARY_PATH="$HOME/anaconda3/envs/icenet/lib:$LD_LIBRARY_PATH"
+# (use after activating the conda environment)
+export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:$LD_LIBRARY_PATH"
--- setenv_IC.sh
+++
@@ -1,3 +0,0 @@
-# Set library paths
-
-export LD_LIBRARY_PATH="/vols/cms/mmieskol/anaconda3/envs/icenet/lib:$LD_LIBRARY_PATH"
