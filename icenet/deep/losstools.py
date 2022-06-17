2ff5931c61b3f16b067c89b360f2bddebce3513f
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2022-06-17 04:29:21 +0100

    construct VAE functions

--- icenet/deep/losstools.py
+++ icenet/deep/losstools.py
@@ -27,14 +27,14 @@ def loss_wrapper(model, x, y, num_classes, weights, param):
         y = y.type(torch.int64)
         return multiclass_focal_entropy_logprob(log_phat=log_phat, y=y, num_classes=num_classes, weights=weights, gamma=param['gamma'])
 
-    elif param['lossfunc'] == 'vae_background_l2':
+    elif param['lossfunc'] == 'VAE_background_only':
         ind  = (y == 0) # Use only background to train
 
         xhat = model.forward(x[ind, ...]) 
         MSE  = torch.sum((xhat - x[ind, ...])**2, dim=-1).mean(dim=0)
-        KL   = model.encoder.kl.mean(dim=0)
+        KL   = torch.sum(model.encoder.kl_i, dim=-1).mean(dim=0)
         
-        return MSE + KL
+        return MSE + param['VAE_beta']*KL
 
     else:
         print(__name__ + f".loss_wrapper: Error with unknown lossfunc {param['lossfunc']}")
--- icenet/deep/vae.py
+++ icenet/deep/vae.py
@@ -1,6 +1,7 @@
 # Variational autoencoder
 # 
 # https://arxiv.org/abs/1312.6114
+# https://lilianweng.github.io/posts/2018-08-12-vae/
 #
 # m.mieskolainen@imperial.ac.uk, 2022
 
@@ -27,44 +28,42 @@ Training loop ELBO minimization:
 """
 
 class Encoder(nn.Module):
-    def __init__(self, D, hidden_dim=128, latent_dim=32):
+    def __init__(self, D, hidden_dim=128, latent_dim=32, activation='tanh'):
         super(Encoder, self).__init__()
-        self.linear1 = nn.Linear(D, hidden_dim)
-        self.linear2 = nn.Linear(hidden_dim, latent_dim)
+
+        self.mlp = MLP([D, hidden_dim, latent_dim], activation=activation)
 
         # modules  = [self.linear2, torch.sigmoid()]
         #self.linear2 = nn.Sequential(*modules)
 
     def forward(self, x):
-        x = torch.flatten(x, start_dim=1)
-        x = F.relu(self.linear1(x))
-        return self.linear2(x)
+        return self.mlp(x)
 
 class VariationalEncoder(nn.Module):
-    def __init__(self, D, hidden_dim, latent_dim):
+    def __init__(self, D, hidden_dim, latent_dim, activation='tanh'):
         super(VariationalEncoder, self).__init__()
         
-        self.mlp        = MLP([D, hidden_dim, hidden_dim], activation='relu')
+        self.mlp        = MLP([D, hidden_dim, hidden_dim], activation=activation)
 
-        self.mlp_mu     = MLP([hidden_dim, hidden_dim, latent_dim], activation='tanh')
-        self.mlp_logvar = MLP([hidden_dim, hidden_dim, latent_dim], activation='tanh')
+        self.mlp_mu     = MLP([hidden_dim, latent_dim], activation=activation)
+        self.mlp_logvar = MLP([hidden_dim, latent_dim], activation=activation)
 
         self.N          = torch.distributions.Normal(0, 1)
         self.kl = 0
 
     def forward(self, x):
+        q        = self.mlp(x)
 
-        q       = self.mlp(x)
+        # "Re-parametrization" trick to allow gradients backpropagate
+        mu       = self.mlp_mu(q)
+        logvar   = self.mlp_logvar(q)
+        std      = torch.exp(0.5 * logvar)
 
-        # "Re-parametrization" trick
-        mu      = self.mlp_mu(q)
-        logvar  = self.mlp_logvar(q)
-        std     = torch.exp(0.5 * logvar)
+        z        = mu + std*self.N.sample(mu.shape)
 
-        z       = mu + std*self.N.sample(mu.shape)
-
-        # KL-divergence between unit Gaussian (Wikipedia)
-        self.kl = 0.5 * torch.sum(std.pow(2) + mu.pow(2) - 1 - logvar, dim=-1)
+        # Analytic KL-divergence against unit Gaussian (Wikipedia)
+        # Take torch.sum(self.kl_i, dim=-1) to obtain the MC estimate
+        self.kl_i = 0.5 * (std.pow(2) + mu.pow(2) - 1 - logvar)
 
         return z
 
@@ -77,24 +76,25 @@ class VariationalEncoder(nn.Module):
 
 
 class Decoder(nn.Module):
-    def __init__(self, latent_dim, hidden_dim, D):
+    def __init__(self, latent_dim, hidden_dim, D, activation='tanh'):
         super(Decoder, self).__init__()
 
-        self.mlp = MLP([latent_dim, hidden_dim, hidden_dim, D], activation='relu')
+        self.mlp = MLP([latent_dim, hidden_dim, hidden_dim, D], activation=activation)
 
     def forward(self, z):
         return self.mlp(z)
 
 
 class VAE(nn.Module):
-    def __init__(self, D, latent_dim, hidden_dim, C=None):
+    def __init__(self, D, latent_dim, hidden_dim, encoder_act='tanh', decoder_act='tanh', anomaly_score='mse', C=None):
         super(VAE, self).__init__()
 
         self.D       = D
         self.C       = C
+        self.anomaly_score = anomaly_score
 
-        self.encoder = VariationalEncoder(D=D, hidden_dim=hidden_dim, latent_dim=latent_dim)
-        self.decoder = Decoder(latent_dim=latent_dim, hidden_dim=hidden_dim, D=D)
+        self.encoder = VariationalEncoder(D=D, hidden_dim=hidden_dim, latent_dim=latent_dim, activation=encoder_act)
+        self.decoder = Decoder(latent_dim=latent_dim, hidden_dim=hidden_dim, activation=decoder_act, D=D)
 
     def to_device(self, device):
         self.encoder = self.encoder.to(device)
@@ -108,13 +108,17 @@ class VAE(nn.Module):
         return xhat
 
     def softpredict(self, x):
-        xhat  = self.forward(x)
-        mse   = torch.sum((xhat - x)**2 / x.shape[-1], dim=-1)
+        z     = self.encoder(x)
+        xhat  = self.decoder(z)
         
-        # "test statistic"
-        score = mse
+        # "Test statistic"
+        if   self.anomaly_score == 'mse':
+            score = torch.sum((xhat - x)**2 / x.shape[-1], dim=-1)
+        elif self.anomaly_score == 'var_kl':
+            score = torch.var(self.encoder.kl_i, dim=-1) / z.shape[-1]
 
         if self.training:
             return torch.log(torch.tanh(score)) # Numerically more stable
         else:
             return torch.tanh(score)
+
