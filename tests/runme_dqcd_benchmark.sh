c393d7025dd02379b208fa0f753cde0bc316eae7
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2022-07-06 08:01:46 +0100

    update scripts

--- tests/runme_dqcd_benchmark.sh
+++ tests/runme_dqcd_benchmark.sh
@@ -8,36 +8,39 @@
 ## Process input
 
 DATAPATH="/home/user/travis-stash/input/icedqcd"
+CONFIG="tune0.yml"
 CMD="python analysis/dqcd.py"
 
 if [ ${maxevents+x} ]; then MAX="--maxevents $maxevents"; else MAX=""; fi
 
-$CMD $MAX --runmode genesis --config tune0.yml --inputmap mc_input.yml   --datapath $DATAPATH
-$CMD $MAX --runmode genesis --config tune0.yml --inputmap mc_input_1.yml --datapath $DATAPATH
-$CMD $MAX --runmode genesis --config tune0.yml --inputmap mc_input_2.yml --datapath $DATAPATH
+$CMD $MAX --runmode genesis --config $CONFIG --inputmap mc_input.yml   --datapath $DATAPATH
+$CMD $MAX --runmode genesis --config $CONFIG --inputmap mc_input_1.yml --datapath $DATAPATH
+$CMD $MAX --runmode genesis --config $CONFIG --inputmap mc_input_2.yml --datapath $DATAPATH
 
 
 # --------------------
 ## Train models
 
-$CMD $MAX --runmode train --config tune0.yml --inputmap mc_input.yml   --modeltag pointANY --use_conditional 1
-$CMD $MAX --runmode train --config tune0.yml --inputmap mc_input_1.yml --modeltag point1
-$CMD $MAX --runmode train --config tune0.yml --inputmap mc_input_2.yml --modeltag point2
+$CMD $MAX --runmode train --config $CONFIG --inputmap mc_input.yml   --modeltag pointANY --use_conditional 1
+$CMD $MAX --runmode train --config $CONFIG --inputmap mc_input_1.yml --modeltag point1
+$CMD $MAX --runmode train --config $CONFIG --inputmap mc_input_2.yml --modeltag point2
 
 
 # --------------------
 ## Evaluate models
 
-$CMD $MAX --runmode eval --config tune0.yml --inputmap mc_input.yml   --modeltag pointANY --use_conditional 1
-$CMD $MAX --runmode eval --config tune0.yml --inputmap mc_input_1.yml --modeltag pointANY --use_conditional 1
-$CMD $MAX --runmode eval --config tune0.yml --inputmap mc_input_2.yml --modeltag pointANY --use_conditional 1
+# Conditional model
+$CMD $MAX --runmode eval --config $CONFIG --inputmap mc_input.yml   --modeltag pointANY --use_conditional 1
+$CMD $MAX --runmode eval --config $CONFIG --inputmap mc_input_1.yml --modeltag pointANY --use_conditional 1
+$CMD $MAX --runmode eval --config $CONFIG --inputmap mc_input_2.yml --modeltag pointANY --use_conditional 1
 
 
-# Single points matched
-$CMD $MAX --runmode eval --config tune0.yml --inputmap mc_input_1.yml --modeltag point1
-$CMD $MAX --runmode eval --config tune0.yml --inputmap mc_input_2.yml --modeltag point2
+# Single points matched model
+$CMD $MAX --runmode eval --config $CONFIG --inputmap mc_input_1.yml --modeltag point1
+$CMD $MAX --runmode eval --config $CONFIG --inputmap mc_input_2.yml --modeltag point2
 
 
-# Single points unmatched
-$CMD $MAX --runmode eval --config tune0.yml --inputmap mc_input_1.yml --modeltag point2
-$CMD $MAX --runmode eval --config tune0.yml --inputmap mc_input_2.yml --modeltag point1
+# Single points unmatched model
+$CMD $MAX --runmode eval --config $CONFIG --inputmap mc_input_1.yml --modeltag point2
+$CMD $MAX --runmode eval --config $CONFIG --inputmap mc_input_2.yml --modeltag point1
+
