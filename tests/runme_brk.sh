118fd9a49279b6bcb06d998914e8b9636bde47f9
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2022-07-09 14:41:13 +0100

    re-organize graph network methods

--- tests/runme_brk.sh
+++ tests/runme_brk.sh
@@ -14,6 +14,6 @@ if [ ${maxevents+x} ]; then MAX="--maxevents $maxevents"; else MAX=""; fi
 # Use * or other glob wildcards for filenames
 
 # Training, Calculation, Analysis
-python analysis/brk_train.py   --runmode all $MAX --config $CONFIG --datapath $DATAPATH --datasets 'BParkNANO_mc_relaxed_Kee_0.root' | tee "./figs/brk/$CONFIG/train_output.txt"
-python analysis/brk_calc.py    --runmode all $MAX --config $CONFIG --datapath $DATAPATH --datasets 'BParkNANO_mc_relaxed_Kee_0.root' | tee "./figs/brk/$CONFIG/calc_output.txt"
-python analysis/brk_analyze.py --runmode all $MAX --config $CONFIG --datapath $DATAPATH --datasets 'BParkNANO_mc_relaxed_Kee_0.root' | tee "./figs/brk/$CONFIG/print_output.txt"
+python analysis/brk_train.py   --runmode all $MAX --config $CONFIG --datapath $DATAPATH --datasets 'BParkNANO_mc_relaxed_Kee_0.root' #| tee "./figs/brk/$CONFIG/train_output.txt"
+python analysis/brk_calc.py    --runmode all $MAX --config $CONFIG --datapath $DATAPATH --datasets 'BParkNANO_mc_relaxed_Kee_0.root' #| tee "./figs/brk/$CONFIG/calc_output.txt"
+python analysis/brk_analyze.py --runmode all $MAX --config $CONFIG --datapath $DATAPATH --datasets 'BParkNANO_mc_relaxed_Kee_0.root' #| tee "./figs/brk/$CONFIG/print_output.txt"
