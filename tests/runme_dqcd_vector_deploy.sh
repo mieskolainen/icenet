ef2ca5a5401d85e833841e36e102ee9b54ba5ddd
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2022-10-24 15:03:14 +0100

    update deployment

--- tests/runme_dqcd_vector_deploy.sh
+++ tests/runme_dqcd_vector_deploy.sh
@@ -16,11 +16,11 @@ ulimit -v unlimited
 # Use * or other glob wildcards for filenames
 # tee redirect output to both a file and to screen
 
-python configs/dqcd/include/ymlgen.py --process 'QCD' --filerange '[11-100000]' --outputfile configs/dqcd/include/QCD_deploy.yml
-python analysis/dqcd_deploy.py --use_conditional 1 --inputmap 'include/QCD_deploy.yml' --modeltag vector_all --config $CONFIG --datapath $DATAPATH
+#python configs/dqcd/include/ymlgen.py --process 'QCD' --filerange '[11-100000]' --outputfile configs/dqcd/include/QCD_deploy.yml
+#python analysis/dqcd_deploy.py --use_conditional 1 --inputmap 'include/QCD_deploy.yml' --modeltag vector_all --config $CONFIG --datapath $DATAPATH
 
-python configs/dqcd/include/ymlgen.py --process 'vector' --filerange '[0-149]' --outputfile configs/dqcd/include/vector_deploy.yml
-python analysis/dqcd_deploy.py --use_conditional 1 --inputmap 'include/vector_deploy.yml' --modeltag vector_all --config $CONFIG --datapath $DATAPATH
+#python configs/dqcd/include/ymlgen.py --process 'vector' --filerange '[0-149]' --outputfile configs/dqcd/include/vector_deploy.yml
+#python analysis/dqcd_deploy.py --use_conditional 1 --inputmap 'include/vector_deploy.yml' --modeltag vector_all --config $CONFIG --datapath $DATAPATH
 
 python configs/dqcd/include/ymlgen.py --process 'data' --filerange '[0-100000]' --outputfile configs/dqcd/include/data_deploy.yml
 python analysis/dqcd_deploy.py --use_conditional 1 --inputmap 'include/data_deploy.yml' --modeltag vector_all --config $CONFIG --datapath $DATAPATH
