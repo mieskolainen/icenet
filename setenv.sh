af8a604def3513ad11335fcfbb474466175265d1
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2023-04-09 17:23:11 +0100

    update scripts [no ci]

--- setenv.sh
+++ setenv.sh
@@ -2,11 +2,19 @@
 # (use after activating the conda environment)
 export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:$LD_LIBRARY_PATH"
 
+echo $LD_LIBRARY_PATH
+
+echo "Before:"
+echo " ulimit -s: $(ulimit -s) (stack)" 
+echo " ulimit -u: $(ulimit -u) (threads)" 
+echo " ulimit -v: $(ulimit -v) (virtual memory)" 
+
 # Set system memory limits
 ulimit -s unlimited  # stack
 ulimit -u 131072     # num of threads (for Sun Grid Engine use)
 #ulimit -v unlimited # virtual memory
 
-echo "ulimit -s: $(ulimit -s)" 
-echo "ulimit -u: $(ulimit -u)" 
-echo "ulimit -v: $(ulimit -v)" 
+echo "After:"
+echo " ulimit -s: $(ulimit -s)" 
+echo " ulimit -u: $(ulimit -u)" 
+echo " ulimit -v: $(ulimit -v)" 
