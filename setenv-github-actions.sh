5bdff1ac2bdd75c6038ca9627cb0601911ebfc1e
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2023-04-09 17:07:19 +0100

    update

--- setenv-github-actions.sh
+++ setenv-github-actions.sh
@@ -4,5 +4,5 @@ export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:$LD_LIBRARY_PATH"
 
 # Set system memory limits
 ulimit -s unlimited  # stack
-ulimit -u 131072     # num of threads (for Sun Grid Engine use)
+#ulimit -u 131072     # num of threads
 #ulimit -v unlimited # virtual memory
