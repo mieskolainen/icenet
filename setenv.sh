cd5c8c13e7e13d3e34fde62fae929b9e90a11b3d
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2023-04-09 19:05:56 +0100

    update scripts [no ci]

--- setenv.sh
+++ setenv.sh
@@ -5,16 +5,13 @@ export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:$LD_LIBRARY_PATH"
 echo $LD_LIBRARY_PATH
 
 echo "Before:"
-echo " ulimit -s: $(ulimit -s) (stack)" 
-echo " ulimit -u: $(ulimit -u) (threads)" 
-echo " ulimit -v: $(ulimit -v) (virtual memory)" 
+ulimit -a
 
 # Set system memory limits
 ulimit -s unlimited  # stack
-ulimit -u 131072     # num of threads (for Sun Grid Engine use)
+ulimit -u 1048576    # num of processes (for Sun Grid Engine use)
 #ulimit -v unlimited # virtual memory
 
+echo ""
 echo "After:"
-echo " ulimit -s: $(ulimit -s)" 
-echo " ulimit -u: $(ulimit -u)" 
-echo " ulimit -v: $(ulimit -v)" 
+ulimit -a
