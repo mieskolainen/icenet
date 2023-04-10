9560d33a6181474a29a8e21686103dc00238f7dc
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2023-04-10 13:18:12 +0100

    adjust environment setup

--- setenv.sh
+++ setenv.sh
@@ -17,13 +17,16 @@ echo "After:"
 ulimit -a
 echo ""
 
+export GRID_ID=0
+export GRID_NODES=1
+
 # Sun Grid Engine array job environment variables
 if [ ${SGE_TASK_ID} ]
 then
     export GRID_ID=$(expr ${SGE_TASK_ID} - 1)
     export GRID_NODES=${SGE_TASK_LAST}
-
-    echo "GRID_ID=${GRID_ID}"
-    echo "GRID_NODES=${GRID_NODES}"
-    echo ""
 fi
+
+echo "GRID_ID=${GRID_ID}"
+echo "GRID_NODES=${GRID_NODES}"
+echo ""
