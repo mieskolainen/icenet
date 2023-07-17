f1cdb2d89536c30d3f6d30f82baf4eb9b096ef0a
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2023-07-17 22:36:48 +0100

    add new tests

--- tests/runme_dqcd_vector_train.sh
+++ tests/runme_dqcd_vector_train.sh
@@ -12,13 +12,19 @@ cd $ICEPATH
 echo "$(pwd)"
 source $ICEPATH/setenv.sh
 
-CONFIG="tune0.yml"
+CONFIG="tune0-dxy.yml"
 DATAPATH="/vols/cms/mc3909"
 
 CONDITIONAL=1
 MAX=1000000 # Tune according to maximum CPU RAM available
 
-python analysis/dqcd.py --runmode genesis  --maxevents $MAX --inputmap mc_map__vector_all.yml --modeltag vector_all --config $CONFIG --datapath $DATAPATH
-python analysis/dqcd.py --runmode train    --maxevents $MAX --inputmap mc_map__vector_all.yml --modeltag vector_all --config $CONFIG --datapath $DATAPATH --use_conditional $CONDITIONAL
-python analysis/dqcd.py --runmode eval     --maxevents $MAX --inputmap mc_map__vector_all.yml --modeltag vector_all --config $CONFIG --datapath $DATAPATH --use_conditional $CONDITIONAL
-python analysis/dqcd.py --runmode optimize --maxevents $MAX --inputmap mc_map__vector_all.yml --modeltag vector_all --config $CONFIG --datapath $DATAPATH --use_conditional $CONDITIONAL
+for CONFIG in "tune0-dxy.yml" "tune0-dlen.yml" "tune0-chi2.yml"
+do
+
+    rm $ICEPATH/output/dqcd/processed_data_*
+    
+    python analysis/dqcd.py --runmode genesis  --maxevents $MAX --inputmap mc_map__vector_all.yml --modeltag vector_all --config $CONFIG --datapath $DATAPATH
+    python analysis/dqcd.py --runmode train    --maxevents $MAX --inputmap mc_map__vector_all.yml --modeltag vector_all --config $CONFIG --datapath $DATAPATH --use_conditional $CONDITIONAL
+    python analysis/dqcd.py --runmode eval     --maxevents $MAX --inputmap mc_map__vector_all.yml --modeltag vector_all --config $CONFIG --datapath $DATAPATH --use_conditional $CONDITIONAL
+    python analysis/dqcd.py --runmode optimize --maxevents $MAX --inputmap mc_map__vector_all.yml --modeltag vector_all --config $CONFIG --datapath $DATAPATH --use_conditional $CONDITIONAL
+done
