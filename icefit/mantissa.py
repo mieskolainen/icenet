8f36216b71f593d29458c4cc39cb99630fb6eefa
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2023-08-02 00:22:55 +0100

    Mantissa bit reduction simulation

---
+++ icefit/mantissa.py
@@ -0,0 +1,198 @@
+# Floating point mantissa bit reduction precision impact study
+# 
+# run with: python icefit/mantissa.py
+#
+# m.mieskolainen@imperial.ac.uk, 2023
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+plt.switch_backend('agg') # matplotlib thread problems
+
+import math
+import numba
+import copy
+import math
+import os
+
+@numba.njit
+def round_sig(x: float, sig: int=2):
+    """
+    Round to significant figures
+    """
+    return round(x, sig-int(math.floor(math.log10(abs(x))))-1)
+
+@numba.njit
+def reduce_mantissa_digits(x: np.ndarray, mandigits: int=3):
+    """
+    Args:
+        x         : floating point numbers in an array
+        mandigits : number of digits precision in the mantissa
+    """
+    y = np.zeros(len(x))
+    
+    for i in range(len(x)):
+        
+        # Get mantissa and exponent
+        m,e  = math.frexp(x[i])
+        # Round mantissa
+        m    = np.round(m, mandigits)
+        # Map back
+        y[i] = math.ldexp(m,e)
+
+    return y
+
+@numba.njit
+def reduce_mantissa_bits(x: np.ndarray, manbits: int=10):
+    """
+    Args:
+        x       : floating point numbers in an array
+        manbits : number of leading bits for the mantissa
+    """
+    y = np.zeros(len(x))
+    
+    for i in range(len(x)):
+        
+        # Get mantissa and exponent
+        m,e  = math.frexp(x[i])
+        # Scale mantissa with ldexp, then round to nearest integer
+        mhat = np.rint(math.ldexp(m, manbits))
+        # Scale back and recover the exponent
+        y[i] = math.ldexp(mhat, e - manbits)
+    
+    return y
+
+def digits2bits(digits: float) -> float:
+    return digits * np.log(10) / np.log(2)
+
+def bits2digits(bits: int) -> float:
+    return np.log10(2**bits)
+
+def plots(x, xhat, b, error_bins=300, dquant_label='none', mantissa_title='None', label='default', savepath='output'):
+
+    # Error distribution plots
+    fig,ax = plt.subplots(1,2, figsize=(8,12))
+
+    plt.sca(ax[0])
+    plt.hist( x - xhat, error_bins)
+    plt.xlabel('$x - \\tilde{x}$')
+    plt.title(f'Error (dequantize: {dquant_label})')
+    
+    plt.sca(ax[1])
+    plt.hist( (x - xhat) / x, error_bins)
+    plt.xlabel('$(x - \\tilde{x}) / x$')
+    plt.title(f'Relative error (dequantize: {dquant_label})')
+    
+    for i in range(len(ax)):
+        ax[i].set_aspect(1.0/ax[i].get_data_ratio(), adjustable='box')
+        
+    filename = f'{savepath}/hist_error_{label}.pdf'
+    print(f'Saving figure to: {filename}')
+    fig.savefig(filename, bbox_inches='tight')
+    plt.close()
+    
+    # Histograms
+    fig,ax = plt.subplots(3, len(b), figsize=(16,14))
+    fig.tight_layout(pad=6.0) # Give space
+    
+    for i in range(len(b)):
+        
+        bins  = b[i]
+        
+        plt.sca(ax[0][i])
+        n_x, _, _ = plt.hist(x, bins=bins)
+        plt.title('IEEE 64b floats')
+        plt.ylabel(f'counts / {bins[1]-bins[0]:0.2g} bin')
+        plt.xlabel('$x$')
+        
+        plt.sca(ax[1][i])
+        n_xhat, _, _ = plt.hist(xhat, bins=bins)
+        plt.title(mantissa_title)
+        plt.ylabel(f'counts / {bins[1]-bins[0]:0.2g} bin')
+        plt.xlabel('$\\tilde{x}$')
+        
+        plt.sca(ax[2][i])
+        plt.plot(bins, np.ones(len(bins)), color=(0.75,0.75,0.75)) # Horizontal line
+        plt.step((bins[1:] + bins[:-1])/2, n_xhat / np.clip(n_x, a_min=1E-15, a_max=None))
+        plt.title('Ratio')
+        plt.ylabel('$\\tilde{N} \, / \, N$')
+    
+    for i in range(len(ax)):
+        for j in range(len(ax[i])):
+            ax[i][j].set_aspect(1.0/ax[i][j].get_data_ratio(), adjustable='box')
+
+    filename = f'{savepath}/hist_counts_{label}.pdf'
+    print(f'Saving figure to: {filename}')
+    fig.savefig(filename, bbox_inches='tight')
+    plt.close()
+
+
+def main():
+
+    savepath = os.getcwd() + '/output/mantissa'
+    
+    if not os.path.exists(savepath):
+        os.makedirs(savepath)
+    
+    # Number of samples
+    N = int(1e6)
+
+    ## Histogram binning
+    min_edge = 1.8
+    max_edge = 2.2
+
+    b = [np.linspace(min_edge, max_edge, 501),
+         np.linspace(min_edge, max_edge, 51),
+         np.linspace(min_edge, max_edge, 26),
+         np.linspace(min_edge, max_edge, 11)]
+
+    # Dequantize strength parameter
+    d_sigma = 1e-3
+
+    # MC simulation input
+    for input in ['flat', 'resonance']:
+        
+        if input == 'flat':
+            # Generate flat distribution
+            xmin = 0
+            xmax = 5
+            x = xmin + (xmax - xmin) * np.random.rand(N)
+
+        elif input == 'resonance':    
+            # Generate Gaussian like resonance
+            mu    = 2
+            sigma = 0.05
+            x     = mu + sigma * np.random.randn(N)
+
+        # Number of mantissa bits to preserve
+        for manbits in [10, 15, 20]:
+            
+            mandigits = bits2digits(manbits)
+            mantissa_title = f'{manbits} bits ~ mantissa {mandigits:0.1f} digits'
+
+            print(mantissa_title)
+            
+            # Reduce floating point precision
+            print(f'Reducing precision to {manbits} mantissa bits')
+            xhat_raw = reduce_mantissa_bits(x, manbits)
+
+            # Post-Dequantization diagnostics
+            for dquant in ['none']:
+                
+                if dquant == 'none':
+                    xhat = copy.deepcopy(xhat_raw)
+                
+                # Add relative Gaussian dithering noise to dequantize
+                if dquant == 'gaussian':
+                    xhat = xhat_raw * (1 + d_sigma * np.random.randn(N))
+                
+                # Make plots
+                plots(x=x, xhat=xhat, b=b, dquant_label=dquant, \
+                    mantissa_title=mantissa_title, label=f'MC-input_{input}_manbits_{manbits}_dquant_{dquant}', \
+                    savepath=savepath)
+
+
+if __name__ == "__main__":
+    main()
+
+    
