225035af37863ba570ee13d97f0988da2a1ea82f
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2024-03-07 10:57:41 +0000

    clean-up steering files

--- tests/runme_dqcd_vector_init_yaml.sh
+++ tests/runme_dqcd_vector_init_yaml.sh
@@ -18,11 +18,12 @@ source $ICEPATH/setenv.sh
 python configs/dqcd/include/ymlgen.py --paramera 'old' --process 'QCD'    --filerange '[0-50]'      --outputfile configs/dqcd/include/QCD.yml
 python configs/dqcd/include/ymlgen.py --paramera 'old' --process 'vector' --filerange '[0-10]'      --outputfile configs/dqcd/include/vector.yml
 
+# Training (Domain Adaptation)
+python configs/dqcd/include/ymlgen.py --paramera 'old' --process 'data-D' --filerange '[0-10]'      --outputfile configs/dqcd/include/data_DA.yml
+
 # Deployment
 python configs/dqcd/include/ymlgen.py --paramera 'old' --process 'QCD'    --filerange '[51-100000]' --outputfile configs/dqcd/include/QCD_deploy.yml
 python configs/dqcd/include/ymlgen.py --paramera 'old' --process 'vector' --filerange '[11-100000]' --outputfile configs/dqcd/include/vector_deploy.yml
 python configs/dqcd/include/ymlgen.py --paramera 'old' --process 'data-B' --filerange '*'           --outputfile configs/dqcd/include/data-B_deploy.yml
 python configs/dqcd/include/ymlgen.py --paramera 'old' --process 'data-C' --filerange '*'           --outputfile configs/dqcd/include/data-C_deploy.yml
 python configs/dqcd/include/ymlgen.py --paramera 'old' --process 'data-D' --filerange '*'           --outputfile configs/dqcd/include/data-D_deploy.yml
-
-
