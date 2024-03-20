dc25fd5b017aaeca552acf41f59b6904a9d05a0d
Rob Bainbridge <bainbrid@users.noreply.github.com>
2024-03-20 12:16:15 +0000

    move -f -r args before dir name

--- superclean.sh
+++ superclean.sh
@@ -4,7 +4,7 @@
 echo "Cleaning output, figs, checkpoint and tmp folders completely (all results gone) -- Are you sure?"
 select yn in "Yes" "No"; do
     case $yn in
-        Yes ) rm output/* -f -r; rm figs/* -f -r; rm checkpoint/* -f -r; rm tmp/* -f -r; break;;
+        Yes ) rm -f -r output/*; rm -f -r figs/*; rm -f -r checkpoint/*; rm -f -r tmp/*; break;;
         No ) break;;
     esac
 done
