99b87d12e4c6178af75df9d1c8eece85175b94f6
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2024-05-28 17:09:54 +0100

    clean-up steering files

--- tests/runme_dqcd_vector_train_DA.sh
+++ tests/runme_dqcd_vector_train_DA.sh
@@ -18,7 +18,7 @@ DATAPATH="/vols/cms/khl216"
 CONDITIONAL=1
 MAX=5000000    # Tune according to maximum CPU RAM available
 
-#python analysis/dqcd.py --runmode genesis  --maxevents $MAX --inputmap mc_map__vector_all_DA.yml --config $CONFIG --datapath $DATAPATH
-#python analysis/dqcd.py --runmode train    --maxevents $MAX --inputmap mc_map__vector_all_DA.yml --modeltag vector_all --config $CONFIG --datapath $DATAPATH --use_conditional $CONDITIONAL
+python analysis/dqcd.py --runmode genesis  --maxevents $MAX --inputmap mc_map__vector_all_DA.yml --config $CONFIG --datapath $DATAPATH
+python analysis/dqcd.py --runmode train    --maxevents $MAX --inputmap mc_map__vector_all_DA.yml --modeltag vector_all --config $CONFIG --datapath $DATAPATH --use_conditional $CONDITIONAL
 python analysis/dqcd.py --runmode eval     --maxevents $MAX --inputmap mc_map__vector_all_DA.yml --modeltag vector_all --config $CONFIG --datapath $DATAPATH --use_conditional $CONDITIONAL
 python analysis/dqcd.py --runmode optimize --maxevents $MAX --inputmap mc_map__vector_all_DA.yml --modeltag vector_all --config $CONFIG --datapath $DATAPATH --use_conditional $CONDITIONAL
