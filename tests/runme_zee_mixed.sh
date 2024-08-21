467ff4fad532f6c3d88a63a2eac84bb0c4c12ac5
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2024-08-21 09:56:11 +0100

    add more complex dataflow steering options

--- tests/runme_zee_mixed.sh
+++ tests/runme_zee_mixed.sh
@@ -13,8 +13,8 @@ CONFIG="tune0_EEp.yml"
 
 # Step 1. Produce output (cache) files with all sign events
 python analysis/zee.py --runmode "genesis" $MAX --config $CONFIG --datapath $DATAPATH --supertune "drop_negative=False"
-python analysis/zee.py --runmode "train"   $MAX --config $CONFIG --datapath $DATAPATH --compute 0
-python analysis/zee.py --runmode "eval"    $MAX --config $CONFIG --datapath $DATAPATH --compute 0
+python analysis/zee.py --runmode "train"   $MAX --config $CONFIG --datapath $DATAPATH --supertune "drop_negative=False" --compute 0
+python analysis/zee.py --runmode "eval"    $MAX --config $CONFIG --datapath $DATAPATH --supertune "drop_negative=False" --compute 0
 
 # Step 2. Default training with only positive sign events
 python analysis/zee.py --runmode "genesis" $MAX --config $CONFIG --datapath $DATAPATH
@@ -23,11 +23,11 @@ python analysis/zee.py --runmode "eval"    $MAX --config $CONFIG --datapath $DAT
 
 # ** Change these manually by carefully inspecting outputs under /output and /checkpoint **
 
-# These correspond to Step 1.
+# These correspond to Step 1. above
 HASH="KA14gKO82bSegMkRSDE5anR10PzJn3mD6Yk8B9Qg5PE="
 POST_HASH="KA14gKO82bSegMkRSDE5anR10PzJn3mD6Yk8B9Qg5PE=__jnjX7Oa_HEMM4WH4J1otCnCFoRl1TDIn_6Gx7mh4sz8="
 
-# This corresponds to Step 2.
+# This corresponds to Step 2. training above
 RUN_ID="2024-08-08_20-27-57_lxcgpu00"
 
 # Step 3. Evaluation with all sign events
