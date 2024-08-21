5104c601dea8a41d58f28c8e8fa8c34349b1db6a
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2024-08-21 10:17:07 +0100

    add more complex dataflow steering options

--- tests/runme_zee_mixed.sh
+++ tests/runme_zee_mixed.sh
@@ -2,34 +2,34 @@
 #
 # Execute training and evaluation for the ZEE
 #
-# Run with: source runme.sh
+# Run with: ./runme.sh
 
-#DATAPATH="/vols/cms/pfk18/phd/hgg/Jul23/NN21July/N/validations/outputs/Csplit_Jsamp/files"
+# DATAPATH="/vols/cms/pfk18/phd/hgg/Jul23/NN21July/N/validations/outputs/Csplit_Jsamp/files"
 DATAPATH="./travis-stash/input/icezee"
 
-if [ ${maxevents+x} ]; then MAX="--maxevents $maxevents"; else MAX=""; fi
+if [ "${maxevents+x}" ]; then MAX="--maxevents $maxevents"; else MAX=""; fi
 
-CONFIG="tune0_EEp.yml"
+CONFIG="tune0_EEp"
 
 # Step 1. Produce output (cache) files with all sign events
-python analysis/zee.py --runmode "genesis" $MAX --config $CONFIG --datapath $DATAPATH --supertune "drop_negative=False"
-python analysis/zee.py --runmode "train"   $MAX --config $CONFIG --datapath $DATAPATH --compute 0
-python analysis/zee.py --runmode "eval"    $MAX --config $CONFIG --datapath $DATAPATH --compute 0
+python analysis/zee.py --runmode "genesis" $MAX --config "${CONFIG}.yml" --datapath $DATAPATH --hash_genesis "S1-${CONFIG}"                                           --run_id "S1-run-${CONFIG}" --supertune "drop_negative=False"
+python analysis/zee.py --runmode "train"   $MAX --config "${CONFIG}.yml" --datapath $DATAPATH --hash_genesis "S1-${CONFIG}" --hash_post_genesis "S1-${CONFIG}__train" --run_id "S1-run-${CONFIG}" --compute 0
+python analysis/zee.py --runmode "eval"    $MAX --config "${CONFIG}.yml" --datapath $DATAPATH --hash_genesis "S1-${CONFIG}" --hash_post_genesis "S1-${CONFIG}__eval"  --run_id "S1-run-${CONFIG}" --compute 0
 
 # Step 2. Default training with only positive sign events
-python analysis/zee.py --runmode "genesis" $MAX --config $CONFIG --datapath $DATAPATH
-python analysis/zee.py --runmode "train"   $MAX --config $CONFIG --datapath $DATAPATH
-python analysis/zee.py --runmode "eval"    $MAX --config $CONFIG --datapath $DATAPATH
+python analysis/zee.py --runmode "genesis" $MAX --config "${CONFIG}.yml" --datapath $DATAPATH --run_id "S2-run-${CONFIG}"
+python analysis/zee.py --runmode "train"   $MAX --config "${CONFIG}.yml" --datapath $DATAPATH --run_id "S2-run-${CONFIG}"
+python analysis/zee.py --runmode "eval"    $MAX --config "${CONFIG}.yml" --datapath $DATAPATH --run_id "S2-run-${CONFIG}"
 
 # ** Change these manually by carefully inspecting outputs under /output and /checkpoint **
+# However, we use the forced setup above.
 
-# These correspond to Step 1. above
-HASH="KA14gKO82bSegMkRSDE5anR10PzJn3mD6Yk8B9Qg5PE="
-POST_HASH="KA14gKO82bSegMkRSDE5anR10PzJn3mD6Yk8B9Qg5PE=__jnjX7Oa_HEMM4WH4J1otCnCFoRl1TDIn_6Gx7mh4sz8="
+# Example: These correspond to Step 1. above
+#HASH="KA14gKO82bSegMkRSDE5anR10PzJn3mD6Yk8B9Qg5PE="
+#POST_HASH="KA14gKO82bSegMkRSDE5anR10PzJn3mD6Yk8B9Qg5PE=__jnjX7Oa_HEMM4WH4J1otCnCFoRl1TDIn_6Gx7mh4sz8="
 
-# This corresponds to Step 2. training above
-RUN_ID="2024-08-08_20-27-57_lxcgpu00"
+# Example: This corresponds to Step 2. training above
+#RUN_ID="2024-08-08_20-27-57_lxcgpu00"
 
 # Step 3. Evaluation with all sign events
-python analysis/zee.py --runmode "eval" $MAX --config $CONFIG --datapath $DATAPATH --hash_genesis $HASH --hash_post_genesis $POST_HASH --run_id $RUN_ID --evaltag "mixed-eval"
-
+python analysis/zee.py --runmode "eval" $MAX --config "${CONFIG}.yml" --datapath $DATAPATH --hash_genesis "S1-${CONFIG}" --hash_post_genesis "S1-${CONFIG}__eval" --run_id "S2-run-${CONFIG}" --evaltag "S3__mixed-eval"
