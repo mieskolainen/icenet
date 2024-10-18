f2adbc25da01b56d2bf501c0022a77d543e28b55
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2024-10-19 00:21:23 +0100

    simplify steering script

--- tests/runme_zee.sh
+++ tests/runme_zee.sh
@@ -9,9 +9,9 @@ DATAPATH="./travis-stash/input/icezee"
 
 if [ ${maxevents+x} ]; then MAX="--maxevents $maxevents"; else MAX=""; fi
 
-for CONFIG in "tune0_EEp.yml" # "tune0_EB.yml" "tune0_EEm.yml" 
+for CONFIG in "tune0_EEp" # "tune0_EB" "tune0_EEm" 
 do
-  python analysis/zee.py --runmode "genesis" $MAX --config $CONFIG --datapath $DATAPATH --hash_genesis "minimal_${CONFIG}"
-  python analysis/zee.py --runmode "train"   $MAX --config $CONFIG --datapath $DATAPATH --hash_genesis "minimal_${CONFIG}" --hash_post_genesis "minimal_${CONFIG}__train"
-  python analysis/zee.py --runmode "eval"    $MAX --config $CONFIG --datapath $DATAPATH --hash_genesis "minimal_${CONFIG}" --hash_post_genesis "minimal_${CONFIG}__eval" --evaltag "mytest"
+  python analysis/zee.py --runmode "genesis" $MAX --config "${CONFIG}.yml" --datapath $DATAPATH --hash_genesis "minimal_${CONFIG}"
+  python analysis/zee.py --runmode "train"   $MAX --config "${CONFIG}.yml" --datapath $DATAPATH --hash_genesis "minimal_${CONFIG}" --hash_post_genesis "minimal_${CONFIG}__train"
+  python analysis/zee.py --runmode "eval"    $MAX --config "${CONFIG}.yml" --datapath $DATAPATH --hash_genesis "minimal_${CONFIG}" --hash_post_genesis "minimal_${CONFIG}__eval" --evaltag "mytest"
 done
